741c7aee60764b7efdc7d9974a6ac845212e5186
eddie-m-m <eddie.m.menefee@gmail.com>
2025-09-18 13:57:16 -0700

    Testing PRAW client connection

--- main.py
+++ main.py
@@ -15,14 +15,21 @@ db_url = os.getenv("DATABASE_URL")
 
 
 def main():
-    # reddit_client = praw.Reddit(
-    #     client_id=client_id,
-    #     client_secret=client_secret,
-    #     user_agent=client_user_agent,
-    # )
-    connect_to_db(db_url)
+    try:
+        reddit_client = praw.Reddit(
+            client_id=client_id,
+            client_secret=client_secret,
+            user_agent=client_user_agent,
+        )
+        subreddit = reddit_client.subreddit("dataanalysis")
 
-    print("---------------------------------")
+        for submission in subreddit.top(limit=3):
+            print(f" - {submission.title} (Score: {submission.score})")
+
+    except Exception as e:
+        print(f"error: {e}")
+
+    # connect_to_db(db_url)
 
 
 if __name__ == "__main__":
